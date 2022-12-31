e57e95fb450313fd0c1df1f885e3fdab88f990f7
Erik Hoversten <ehoversten@gmail.com>
2022-12-31 15:56:08 -0500

    fixed issue with practice controller. next up user controller

--- server/controllers/practiceController.js
+++ server/controllers/practiceController.js
@@ -1,16 +1,18 @@
-const { default: mongoose } = require('mongoose');
-const express = require('express');
-const Practice = require('../models');
+// const Practice = require('../models');
+const Practice = require('../models/Practice');
+const mongoose = require('mongoose');
+// const express = require('express');
 
 // get all practice sessions
 const getAllSessions = async (req, res) => {
+
     try {
-        let allSessions = await Practice.find();
-        // let allSessions = await Practice.find({}).sort({ createdAt: -1 });
+        let allSessions = await Practice.find({}).sort({ createdAt: -1 });
         res.status(200).json(allSessions);
     } catch(err) {
         res.status(400).json({ error: err.message });
     }
+
 }
 
 // get a single practice session
@@ -50,13 +52,45 @@ const createSession = async (req, res) => {
 // update a practice session
 const updateSession = async (req, res) => {
     console.log(`ID: ${req.params.id}`);
-    res.send("Practice PUT Route");
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No resource found' });
+    }
+
+    try {
+        let session = await Practice.findOneAndUpdate({_id: id}, { ...req.body });
+
+        if(!session) {
+            return res.status(404).json({ error: 'No resource found' });
+        }
+
+        res.status(200).json(session);
+    } catch(err) {
+        res.status(400).json({error: err.message});
+    }
 }
 
 // delete a practice session
 const deleteSession = async (req, res) => {
     console.log(`ID: ${req.params.id}`);
-    res.send("Practice DELETE Route");
+    const { id } = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No resource found' });
+    }
+
+    try {
+        let session = await Practice.findByIdAndDelete({_id: id});
+
+        if(!session) {
+            return res.status(404).json({ error: 'No resource found' });
+        }
+
+        res.status(200).json(session);
+    } catch(err) {
+        res.status(400).json({error: err.message});
+    }
 }
 
 module.exports = { 
--- server/routes/api/practice.js
+++ server/routes/api/practice.js
@@ -1,3 +1,4 @@
+const router = require('express').Router();
 const Practice = require('../../models/Practice');
 const mongoose = require('mongoose');
 const {
@@ -8,103 +9,20 @@ const {
     deleteSession
 } = require('../../controllers/practiceController');
 
-const router = require('express').Router();
 
 // -- All routes prefixed with '/api/practice'
-// router.get('/', getAllSessions);
-router.get('/', async (req, res) => {
-    try {
-        let allSessions = await Practice.find();
-        // let allSessions = await Practice.find({}).sort({ createdAt: -1 });
-        res.status(200).json(allSessions);
-    } catch(err) {
-        res.status(400).json({ error: err.message });
-    }
-});
-
+router.get('/', getAllSessions);
 
 // -- GET ONE
-// router.get('/:id', getSingleSession);
-router.get('/:id', async (req, res) => {
-    console.log(`ID: ${req.params.id}`);
-    const { id } = req.params;
-
-    if(!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ error: 'No resource found' });
-    }
-
-    try {
-        let session = await Practice.findById(id);
-
-        if(!session) {
-            return res.status(404).json({ error: 'No resource found' });
-        }
-
-        res.status(200).json(session);
-    } catch(err) {
-        res.status(400).json({error: err.message});
-    }
-});
+router.get('/:id', getSingleSession);
 
 // -- CREATE 
-// router.post('/', createSession);
-router.post('/', async (req, res) => {
-    const {title, duration, worked_on } = req.body;
-    console.log(title, duration, worked_on);
-
-    try {
-        const session = await Practice.create({title, duration, worked_on});
-        res.status(201).json(session)
-    } catch(err) {
-        res.status(400).json({error: err.message});
-    }
-});
+router.post('/', createSession);
 
 // -- UPDATE
-// router.put('/:id', updateSession); 
-router.put('/:id', async (req, res) => {
-    console.log(`ID: ${req.params.id}`);
-    const { id } = req.params;
-
-    if(!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ error: 'No resource found' });
-    }
-
-    try {
-        let session = await Practice.findOneAndUpdate({_id: id}, { ...req.body });
-
-        if(!session) {
-            return res.status(404).json({ error: 'No resource found' });
-        }
-
-        res.status(200).json(session);
-    } catch(err) {
-        res.status(400).json({error: err.message});
-    }
-});
+router.put('/:id', updateSession); 
 
 // -- DELETE
-// router.delete('/:id', deleteSession);
-router.delete('/:id', async (req, res) => {
-    console.log(`ID: ${req.params.id}`);
-    const { id } = req.params;
-
-    if(!mongoose.Types.ObjectId.isValid(id)) {
-        return res.status(404).json({ error: 'No resource found' });
-    }
-
-    try {
-        let session = await Practice.findByIdAndDelete({_id: id});
-
-        if(!session) {
-            return res.status(404).json({ error: 'No resource found' });
-        }
-
-        res.status(200).json(session);
-    } catch(err) {
-        res.status(400).json({error: err.message});
-    }
-});
-
+router.delete('/:id', deleteSession);
 
 module.exports = router;
