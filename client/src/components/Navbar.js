8c210be884717647e49df7e0483fdb62268d3984
Erik Hoversten <ehoversten@gmail.com>
2023-01-02 20:39:33 -0500

    added logout hook and functionality

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom';
+import { useLogout } from '../hooks/useLogout';
 
 
 const Navbar = () => {
+    const { logout } = useLogout();
+
+    const handleClick = () => {
+        logout();
+    }
+
     return (
         <header>
             <nav>
                 <h2>Practice Session Tracker</h2>
                 <NavLink to='/'>Home</NavLink>
-                <NavLink to='/register'>Register</NavLink>
-                <NavLink to='/login'>Login</NavLink>
                 <NavLink to='about'>About</NavLink>
+                <NavLink to='register'>Register</NavLink>
+                <NavLink to='login'>Login</NavLink>
+                <div className="logout-btn">
+                    <button onClick={handleClick}>Logout</button>
+                </div>
             </nav>
         </header>
     )
--- client/src/hooks/useLogout.js
+++ client/src/hooks/useLogout.js
@@ -1,7 +1,23 @@
 import { useState, useContext } from 'react';
 import { AuthContext } from '../context/authContext';
+import { useAuthContext } from '../hooks/useAuthContext'
 
 export const useLogout = () => {
     const { setUser } = useContext(AuthContext);
+    // const { dispatch } = useAuthContext();
 
+    const logout = () => {
+        // remove user from local storage
+        localStorage.removeItem('user');
+        console.log('user removed from localStorage');
+
+        // dispatch logout action
+        // dispatch({type: 'LOGOUT'});
+
+        // update user state in context
+        setUser(null);
+        console.log("User Removed from Context");
+    }
+
+    return { logout }
 }
